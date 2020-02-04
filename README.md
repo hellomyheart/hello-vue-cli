4b20442b9ecef4b4d3175fecc13627b792274ae2
申永杰 <stepenshen@foxmail.com>
2020-02-04 06:20:58 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# hello-vue-cli
+
+第一个vue-cli单页应用
---
+++ src/components/Content.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+      内容页，欢迎
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Content.vue"
+    }
+</script>
+
+<style scoped>
+
+</style>
